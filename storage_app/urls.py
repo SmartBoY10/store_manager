8f32003272c07ed9634c98efe75acaa9216eed99
qurol.abdujalilov99 <qurol.abdujalilov99@gmail.com>
2022-03-03 12:06:58 +0500

    Manual detail menus

--- storage_app/urls.py
+++ storage_app/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', ManualView.as_view(), name='storage_home'),
-    path('manual/<int:pk>/', ManualDetailView.as_view(), name='manual_detail')
+    path('manual/<int:pk>/', ManualDetailView.as_view(), name='manual_detail'),
+    path('manual/purchase/', PurchaseView.as_view(), name='purchase'),
+    path('manual/sale/', SaleView.as_view(), name='sale'),
+    path('manual/storages/', StorageView.as_view(), name='storages'),
 ]
--- storage_app/views.py
+++ storage_app/views.py
@@ -12,3 +12,25 @@ class ManualDetailView(View):
     def get(self, request, pk):
         storages = Manual.objects.get(id=pk).storage.all()
         return render(request, "storage_app/manual_detail.html", {"storages": storages})
+
+
+class StorageView(View):
+    def get(self, request):
+        storages = Storage.objects.all()
+        for storage in storages:
+            products = Storage.objects.get(id=storage.id).product.all()
+            d = {storage.name: products}
+        print(d)
+        return render(request, "storage_app/storages.html")
+
+
+class PurchaseView(View):
+    def get(self, request):
+        purchases = Purchase.objects.all()
+        return render(request, "storage_app/purchase.html", {"purchases": purchases})
+
+
+class SaleView(View):
+    def get(self, request):
+        sales = Sale.objects.all()
+        return render(request, "storage_app/sale.html", {"sales": sales})
