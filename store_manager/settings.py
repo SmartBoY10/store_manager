18e590fc3533612747f88f2f00ec7283db261ece
SmartBoY10 <qurol.abdujalilov99@gmail.com>
2022-03-06 11:42:19 +0500

    Added bootstrap

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+    os.path.join(BASE_DIR, 'storage_app/static')
+    ]
 
 MEDIA_URL = '/media/'
 
